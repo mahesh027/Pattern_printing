6bb8dd83f8b7fa399cdc0d0906f09819e63f39e8
K Mahesh <mahesh0352002@gmail.com>
2023-05-19 00:41:10 +0530

    commit

---
+++ pattern2.py
@@ -0,0 +1,9 @@
+#Filled Triangle
+n=int(input("enter no.of rows:"))
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(end=" ")
+    for k in range(0,i+1):
+        print("* ",end=" ")
+    print("\n")
+
