795fbfdfc231ff8be6c64e8f1ee059fd5196b41d
K Mahesh <mahesh0352002@gmail.com>
2023-05-19 01:01:42 +0530

    commit

---
+++ number_pattern1.py
@@ -0,0 +1,13 @@
+n=int(input("enter the no.of rows:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print("\n")
+
+print("----------------------------------")
+
+n=int(input("enter the no.of rows:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print("\n")
